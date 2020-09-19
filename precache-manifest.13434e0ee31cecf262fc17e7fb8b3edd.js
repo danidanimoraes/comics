90d03cd7252df5509779e8fd93300d0761c05673
Daniele de Moraes <danimoraes.barbosa@gmail.com>
2020-09-19 16:28:53 -0300

    Updates

---
+++ precache-manifest.13434e0ee31cecf262fc17e7fb8b3edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b83e6a0fda13c6c7a000f6147e6c1c",
+    "url": "/comics/index.html"
+  },
+  {
+    "revision": "3eabc23026d6debf32ae",
+    "url": "/comics/static/css/main.dcfba989.chunk.css"
+  },
+  {
+    "revision": "791dc90b4add12636ace",
+    "url": "/comics/static/js/2.6f7104b2.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/comics/static/js/2.6f7104b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eabc23026d6debf32ae",
+    "url": "/comics/static/js/main.24e30a0c.chunk.js"
+  },
+  {
+    "revision": "2f2e2a87dc7ced7ff51a",
+    "url": "/comics/static/js/runtime-main.fbf2e304.js"
+  },
+  {
+    "revision": "b344b76d0055b87cfb4bce3ee9a1cd22",
+    "url": "/comics/static/media/marvel.b344b76d.jpg"
+  }
+]);
